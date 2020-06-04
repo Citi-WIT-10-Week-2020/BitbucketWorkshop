c16011217f72d9b2f1acfce45a3c06f3ee0e78ab
Mochi <mochiko@mochi.com>
2020-06-04 15:55:34 -0400

    modified login

---
+++ ATM.java
@@ -0,0 +1,33 @@
+	
+import java.util.*;
+ 
+public class SkeletonATM{
+ 
+    double reserve = 20000.0;
+    double bank_account = 0;
+    int pin = 0000;
+ 
+    public SkeletonATM(){
+        super();
+    }
+    public SkeletonATM(double startAmount){
+ 
+    }
+ 
+    public boolean login(int pin){
+        
+        return this.pin == pin ? true : false;
+    }
+    public double withdraw(double request){
+        return 0;
+    }
+ 
+    public void deposit(double input){
+ 
+    }
+ 
+ 
+    public static void main(String args[]){
+        
+    }
+}
